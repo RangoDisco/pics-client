e1ad58c7068d342abb6b671dc57fc7905cbdff06
Maxime <diasmaxime1@gmail.com>
2022-04-25 21:56:51 +0200

    added spotify link in collection card

--- src/components/CollectionCard.tsx
+++ src/components/CollectionCard.tsx
@@ -1,37 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FC } from "react";
-interface IProps {
-  id: number;
-  title: string;
-  date: Date | string;
-  description: string;
-  previewPics: string[];
-  genre: string;
-  tags: string[];
-}
-const CollectionCard: FC<IProps> = ({
-  id,
-  title,
-  date,
-  description,
-  previewPics,
-  genre,
-  tags,
-}: IProps) => {
-  tags = ["Fourvière", "Printemps"];
-  previewPics = [
-    "https://images.unsplash.com/photo-1602087594298-706ccc894bfd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3024&q=80",
-    "https://images.unsplash.com/photo-1650279902445-ed77fe27ac61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
-    "https://images.unsplash.com/photo-1602087594298-706ccc894bfd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3024&q=80",
-  ];
+import { ChangeEvent, FC, MouseEventHandler } from "react";
+import { ICollection, IPicture } from "../contexts/Pictures/types";
+import { FaSpotify } from "react-icons/fa";
+
+const CollectionCard: FC<ICollection> = (collection) => {
+  const handleSpotifyClick = (event: React.MouseEvent<SVGElement>) => {
+    event.stopPropagation();
+    window.open(collection.music, "_blank");
+  };
   return (
-    <Link href={`/collections/${id}`} passHref>
+    <Link href={`/collections/${collection.id}`} passHref>
       <article className="w-full h-96 transition-colors duration-200 hover:cursor-pointer overflow-hidden">
         <div className="w-full h-3/4 flex flex-row">
-          {previewPics &&
-            previewPics.length > 0 &&
-            previewPics.map((pic, index) => (
+          {collection.pictures &&
+            collection.pictures.length > 0 &&
+            collection.pictures.map((picture: IPicture, index: number) => (
               <div
                 className={`w-1/3 h-full relative border border-solid border-raisinBlack rounded-lg ${
                   index === 0
@@ -43,7 +27,7 @@ const CollectionCard: FC<IProps> = ({
                 key={index}
               >
                 <Image
-                  src={pic}
+                  src={picture.contentUrl}
                   alt="random image"
                   layout="fill"
                   objectFit="cover"
@@ -58,35 +42,42 @@ const CollectionCard: FC<IProps> = ({
               </div>
             ))}
         </div>
-        <div className="px-4 -mt-16 relative z-30">
+        <div className="px-4 -mt-20 relative z-30">
           <div className="bg-raisinBlack p-6 rounded-lg shadow-lg">
             <div className="flex items-baseline">
               <span className="bg-munsellBlue text-xs px-2 inline-block rounded-full uppercase font-semibold tracking-wide">
-                {genre || "Street"}
+                {collection.category || "Street"}
               </span>
-              {tags &&
-                tags.length > 0 &&
-                tags.map((tag, index) => (
+              {collection.tags &&
+                collection.tags.length > 0 &&
+                collection.tags.map((tag, index) => (
                   <div
                     className="ml-2 opacity-70 uppercase text-xs font-semibold tracking-wider"
                     key={index}
                   >
-                    {tag || "Paysage"} {index !== tags.length - 1 && "⋅"}
+                    {tag || "Paysage"}{" "}
+                    {index !== collection.tags.length - 1 && "⋅"}
                   </div>
                 ))}
             </div>
 
             <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">
-              {title || "A random Title"}
+              {collection.title || "A random Title"}
             </h4>
 
             <div className="mt-1">
-              <span className=" text-sm"> {date || "various"}</span>
-            </div>
-            <div className="mt-4">
-              <span className="text-sm">
-                {description || "Une superbe description"}
+              <span className=" text-sm">
+                {collection.date ? collection.date.toString() : "various"}
               </span>
+            </div>
+            <div className="mt-1">
+              <span className="text-sm">{collection.description}</span>
+            </div>
+            <div className="w-full flex justify-end">
+              <FaSpotify
+                className="cursor-pointer hover:text-munsellBlue"
+                onClick={handleSpotifyClick}
+              />
             </div>
           </div>
         </div>
