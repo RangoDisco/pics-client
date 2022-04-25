8720524ab5f92bf059ed25e0560fe95e4dddce18
Maxime <diasmaxime1@gmail.com>
2022-04-25 21:57:38 +0200

    added picture and collection type

---
+++ src/contexts/Pictures/types.ts
@@ -0,0 +1,23 @@
+import { IUser } from "../Users/types";
+export interface IPicture {
+  id: number;
+  title: string;
+  contentUrl: string;
+  location: string;
+  date: Date;
+  author: IUser;
+  creationDate: Date;
+  isActive: boolean;
+}
+
+export interface ICollection {
+  id: number;
+  title: string;
+  date: Date;
+  description: string;
+  pictures: IPicture[];
+  author: IUser;
+  category: string;
+  tags: string[];
+  music: string;
+}
