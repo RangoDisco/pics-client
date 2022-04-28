422895b5e2b19386d041fe5ffb27d82eff3f6650
Maxime <diasmaxime1@gmail.com>
2022-04-28 17:09:57 +0200

    collection skeleton

---
+++ src/components/Cards/Collection/CollectionSkeleton.tsx
@@ -0,0 +1,37 @@
+const CollectionSkeleton = () => {
+  return (
+    <article className="w-full h-96 transition-colors duration-200 hover:cursor-pointer overflow-hidden">
+      <div className="w-full h-3/4 flex flex-row">
+        {[0, 1, 2].map((index: number) => (
+          <div
+            className={`w-1/3 h-full relative border border-solid bg-davysGray border-raisinBlack rounded-lg animate-pulse ${
+              index === 0 ? "z-0 mt-5" : index === 1 ? "z-0 w-2/4" : "z-0 mt-5"
+            }`}
+            key={index}
+          ></div>
+        ))}
+      </div>
+      <div className="px-4 -mt-20 relative z-30">
+        <div className="bg-raisinBlack p-6 rounded-lg shadow-lg">
+          <div className="flex items-center">
+            <span className="bg-davysGray animate-pulse text-xs px-2 inline-block rounded-full uppercase font-semibold tracking-wide h-4 w-1/5" />
+            {[0, 1].map((index) => (
+              <div
+                className="ml-2 uppercase text-xs font-semibold tracking-wider bg-davysGray animate-pulse h-3 w-1/6 rounded-full"
+                key={index}
+              />
+            ))}
+          </div>
+
+          <div className="mt-2 bg-davysGray animate-pulse h-6 rounded-full w-2/5" />
+
+          <div className="mt-2 h-2 bg-davysGray animate-pulse w-1/5 rounded-full" />
+          <div className="mt-2 h-10 bg-davysGray animate-pulse w-4/5 rounded-lg" />
+          <div className="w-full flex justify-end"></div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default CollectionSkeleton;
--- src/pages/collections/index.tsx
+++ src/pages/collections/index.tsx
@@ -1,5 +1,6 @@
 import { FC, useEffect } from "react";
 import CollectionCard from "../../components/Cards/Collection/CollectionCard";
+import CollectionSkeleton from "../../components/Cards/Collection/CollectionSkeleton";
 import { usePics } from "../../contexts/Pictures/PicturesProvider";
 import { ICollection } from "../../contexts/Pictures/types";
 
@@ -19,11 +20,13 @@ const Collections: FC = () => {
         className="p-4 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-6"
         style={{ minHeight: "94.1vh" }}
       >
-        {collections &&
-          collections.length > 0 &&
-          collections.map((collection: ICollection, index: number) => (
-            <CollectionCard {...collection} key={`collection-${index}`} />
-          ))}
+        {collections && collections.length > 0
+          ? collections.map((collection: ICollection, index: number) => (
+              <CollectionCard {...collection} key={`collection-${index}`} />
+            ))
+          : [0, 1, 2, 3, 4, 5, 6, 7].map((index) => (
+              <CollectionSkeleton key={index} />
+            ))}
       </section>
     </div>
   );
