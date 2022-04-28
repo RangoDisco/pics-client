33d2b401eeefd8e057ab663c864e97331356f96c
Maxime <diasmaxime1@gmail.com>
2022-04-28 17:10:04 +0200

    SEO

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "localhost"],
   },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 };
 
 module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 import { useEffect } from "react";
 import PictureCard from "../components/Cards/Picture/PictureCard";
@@ -14,28 +15,34 @@ const Home: NextPage = () => {
   }, [fetchPictures]);
 
   return (
-    <div className="bg-richBlack text-ghostWhite">
-      <section className="p-4 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-6">
-        {pictures?.length > 0 ? (
-          pictures.map((picture) => (
-            <PictureCard
-              key={picture.id}
-              id={picture.id}
-              title={picture.title}
-              url={picture.contentUrl}
-              date={picture.date}
-              location={picture.location}
-            />
-          ))
-        ) : (
-          <>
-            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => (
-              <PictureSkeleton key={index} />
-            ))}
-          </>
-        )}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>Pic-Nic Photos</title>
+        <meta name="description" content="Page photos" />
+      </Head>
+      <div className="bg-richBlack text-ghostWhite">
+        <section className="p-4 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-6">
+          {pictures?.length > 0 ? (
+            pictures.map((picture) => (
+              <PictureCard
+                key={picture.id}
+                id={picture.id}
+                title={picture.title}
+                url={picture.contentUrl}
+                date={picture.date}
+                location={picture.location}
+              />
+            ))
+          ) : (
+            <>
+              {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => (
+                <PictureSkeleton key={index} />
+              ))}
+            </>
+          )}
+        </section>
+      </div>
+    </>
   );
 };
 
