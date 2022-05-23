1b5ce255c341cf726d56db47f4b5602315c84c3c
Maxime <diasmaxime1@gmail.com>
2022-05-23 15:45:31 +0200

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,27 @@
 import { FC } from "react";
 
+import { RiGithubFill, RiEarthFill } from "react-icons/ri";
+
 const Footer: FC = () => {
   return (
     <div className="bg-raisinBlack text-ghostWhite p-10 bottom-0 flex flex-col justify-center items-center z-50">
-      <p>Ouais ouais les photos ouais</p>
-      <div className="flex flex-row">
+      <p>Absolument pas de copyright · {new Date().getUTCFullYear()}</p>
+      <div className="flex flex-row justify-between mt-2">
         <a
           className="transition-all duration-200 hover:text-munsellBlue"
           href="https://github.com/RangoDisco"
           target="_blank"
           rel="noreferrer"
         >
-          Github
+          <RiGithubFill size={26} />
         </a>
-        {"⋅"}
         <a
-          className="transition-all duration-200 hover:text-munsellBlue"
+          className="transition-all duration-200 hover:text-munsellBlue ml-4"
           href="https:maxime-dias.fr"
           target="_blank"
           rel="noreferrer"
         >
-          Site
+          <RiEarthFill size={26} />
         </a>
       </div>
     </div>
