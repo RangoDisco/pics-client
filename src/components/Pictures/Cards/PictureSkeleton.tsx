56eedd3bf81ef00ee742b90db63c8d156584f34d
Maxime <diasmaxime1@gmail.com>
2022-06-12 22:46:03 +0200

    rename gray to grey

--- src/components/Pictures/Cards/PictureSkeleton.tsx
+++ src/components/Pictures/Cards/PictureSkeleton.tsx
@@ -1,11 +1,11 @@
 const PictureSkeleton = () => {
   return (
     <article className="w-full h-96 bg-creme dark:bg-raisinBlack shadow-lg rounded-md">
-      <div className="w-full h-3/4 relative bg:silk dark:bg-davysGray rounded-t-md animate-pulse" />
+      <div className="w-full h-3/4 relative bg:silk dark:bg-davysGrey rounded-t-md animate-pulse" />
       <div className="h-6 rounded-full m-4">
-        <div className="mt-2 bg-creme dark:bg-davysGray rounded-full h-4 w-1/2 animate-pulse" />
-        <div className="ml-1 mt-3 bg-creme dark:bg-davysGray rounded-full h-2 w-1/4 animate-pulse" />
-        <div className="ml-1 mt-2 bg-creme dark:bg-davysGray rounded-full h-2 w-1/4 animate-pulse" />
+        <div className="mt-2 bg-creme dark:bg-davysGrey rounded-full h-4 w-1/2 animate-pulse" />
+        <div className="ml-1 mt-3 bg-creme dark:bg-davysGrey rounded-full h-2 w-1/4 animate-pulse" />
+        <div className="ml-1 mt-2 bg-creme dark:bg-davysGrey rounded-full h-2 w-1/4 animate-pulse" />
       </div>
     </article>
   );
