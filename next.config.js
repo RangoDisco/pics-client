f625096bd76b1705e5c3ad7528189ea4e20cc971
Maxime <diasmaxime1@gmail.com>
2022-07-03 01:50:54 +0200

    updated env setup

--- next.config.js
+++ next.config.js
@@ -11,9 +11,6 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
   experimental: {
     outputStandalone: true,
   },
